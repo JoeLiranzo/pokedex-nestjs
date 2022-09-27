d79efd4adcb58472019f6fff561d3f171b48711d
Joel Liranzo <joeliranzo@gmail.com>
2022-09-26 23:33:40 -0400

    Added pagination and thunder querys.

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from './../common/adapters/axios.adapter';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
 	constructor(
 		@InjectModel(Pokemon.name)
-		private readonly pokemonModel:Model<Pokemon>
+		private readonly pokemonModel:Model<Pokemon>,
+		private readonly http: AxiosAdapter,
 	){}
-
-	private readonly axios:AxiosInstance = axios
 
 	async executeSeed(){
 		await this.pokemonModel.deleteMany({})
@@ -21,8 +20,7 @@ export class SeedService {
 		// return fetch(url).then(res => {
 		// 	return res.json();
 		// });
-
-		const { data } = await this.axios.get<PokeResponse>(url)
+		const data = await this.http.get<PokeResponse>(url)
 		
 		const pokemonToInsert = []
 		data.results.forEach(async({name,url})=>{
